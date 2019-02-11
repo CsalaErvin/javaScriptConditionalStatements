755d390e4b0783536f436c7fa8d395d19ceb792d
Ervin Csala <csala.ervin@gmail.com>
2019-02-11 17:44:10 +0100

    corrected

--- conditionalStatements.js
+++ conditionalStatements.js
@@ -1,10 +1,12 @@
-var number1 = 4;
-var number2 = 15;
+var number1 = prompt('Enter a number:');
+var number2 = prompt('Enter another number:');
 
 if (number1 < number2) {
-  console.log(number1 + ' less than ' + number2);
+  alert(number1 + ' less than ' + number2);
+} else if (number1 = number2) {
+	alert(number1 + ' is equal to ' + number2);				 
 } else {
-  console.log(number1 + ' more than ' + number2);
+  alert(number1 + ' more than ' + number2);
 }
 
 console.log(' ');
@@ -13,6 +15,8 @@ var age = prompt('Hello! How old are you?');
 
 if (age < 18) {
   alert("This article is only available for people over 18 years of age.");
+} else if (age == 18) {
+	alert("Exactly 18 years old? Congratulations, welcome to adults! Have fun with the article! ");
 } else {
   alert("Thank you! You can read this article.");
 }
